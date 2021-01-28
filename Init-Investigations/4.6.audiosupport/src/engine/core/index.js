82518c24181517f295bc2695428eacfd11cc72e9
Kelvin Sung <ksung@uw.edu>
2021-01-27 16:11:46 -0800

    To work with transition to new file naming convention

--- Init-Investigations/4.6.audiosupport/src/engine/core/index.js
+++
@@ -1,7 +0,0 @@
-import * as core from './core.js'
-import * as gl from './gl.js'
-import * as input from './input.js'
-import * as loop from './loop.js'
-import * as vertexbuffer from './vertexbuffer.js'
-
-export default {gl, input, loop, vertexbuffer, ...core}
--- Init-Investigations/4.6.audiosupport/src/engine/resources/index.js
+++
@@ -1,5 +0,0 @@
-import * as audio from './audio.js'
-import * as text from './text.js'
-import * as xml from './xml.js'
-
-export default {audio, text, xml}
