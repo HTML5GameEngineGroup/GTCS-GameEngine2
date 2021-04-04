a9b944b6a1d38e5552e7bd8ef7f7ca1d1c0d9d38
Kelvin Sung <ksung@uw.edu>
2021-04-04 10:21:45 -0700

    Chapter 6 ready

---
+++ chapter6/6.4.per_pixel_collisions/src/engine/scene.js
@@ -0,0 +1,66 @@
+/*
+ * File: scene.js
+ *
+ * The template for a scene.
+ * 
+ */
+"use strict"
+
+import  * as loop from "./core/loop.js";
+import engine from "./index.js";
+
+const abstractClassError = new Error("Abstract Class")
+const abstractMethodError = new Error("Abstract Method")
+
+class Scene {
+    constructor() {
+        if (this.constructor === Scene) {
+            throw abstractClassError
+        }
+    }
+
+    async start() {
+        await loop.start(this);
+    }
+
+    stop() {
+        loop.stop();
+        this.unload();
+        this.next();
+    }
+
+    next() {
+        // if sub-class does not override this funciton
+        // then, this is the end of the game
+        engine.cleanUp();
+    }
+
+    
+    init() {
+        // initialize the level and load resources (called from GameLoop)
+        // throw abstractMethodError
+    }
+
+    load() { 
+        // to load necessary resources
+    } 
+
+    unload() {
+        // .. unload all resources
+        // throw abstractMethodError
+    }
+
+    // update to be called form EngineCore.GameLoop
+    update() {
+        // when done with this level should call this.stop()
+        throw abstractMethodError
+    }
+
+    // draw to be called from EngineCore.GameLoop
+    draw() {
+        throw abstractMethodError
+    }
+
+}
+
+export default Scene;
